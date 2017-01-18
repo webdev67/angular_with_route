13db2f46dca1a83317f8ef9b90639b50b2de2885
Rami Mikhail <webdev67@gmail.com>
2017-01-18 00:04:13 -0500

    Added sass support.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 var gulp = require('gulp');
 var install = require("gulp-install");
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
+gulp.task('sass', function () {
+  return gulp.src('./public/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./public/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default',['sass','sass:watch'], function() {
   // place code for your default task here
   gulp.src('./package.json')
     .pipe(install());
