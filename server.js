a01dbdc6e65900e8575cef3d569921f3bd3e9047
Rami Mikhail <rami.a.mikhail@ericsson.com>
2017-01-18 10:06:13 -0500

    Updated package.json

--- server.js
+++ server.js
@@ -5,4 +5,5 @@ app.use(express.static('public'));
 app.use('/scripts',express.static('node_modules'));
 app.use('/uib',express.static('node_modules/angular-ui-bootstrap'));
 console.log("App Running on port 3000");
+
 app.listen(3000);
