1403a3277d978f032c4831f0cc7935a7db669560
Rami Mikhail <webdev67@gmail.com>
2017-01-17 23:49:58 -0500

    Added article service.

--- public/js/controllers/articlesController.js
+++ public/js/controllers/articlesController.js
@@ -1,22 +1,16 @@
-var articlesController = app.controller("articlesController", ['$scope', '$http', '$stateParams', '$sce','$rootScope', function($scope, $http, $stateParams, $sce,$rootScope) {
+var articlesController = app.controller("articlesController", ['$scope', '$http', '$stateParams', '$sce', '$rootScope', 'articleService', function($scope, $http, $stateParams, $sce, $rootScope, articleService) {
     $scope.largeArticle = "";
     $scope.keywords = [];
-    $http.get(app.endpoint).then(function(result) {
 
-        if (result.status == 200) {
-            $scope.articles = result.data.payload;
-            console.log($scope.articles);
-        }
-    });
     if (typeof($stateParams.id) !== "undefined") {
-        $http.get(app.endpoint + $stateParams.id).then(function(result) {
+        articleService.getArticle($stateParams.id).then(function(result) {
             $scope.largeArticle = "";
             $scope.keywords = [];
 
             for (var i = 0; i < result.data.text.length; i++) {
 
                 $scope.largeArticle += result.data.text[i].sentence;
-                if (typeof(result.data.text[i].classification)!=="undefined" && result.data.text[i].classification.length > 0) {
+                if (typeof(result.data.text[i].classification) !== "undefined" && result.data.text[i].classification.length > 0) {
                     for (var p = 0; p < result.data.text[i].classification.length; p++) {
                         if ($scope.keywords.indexOf(result.data.text[i].classification[p].key) == -1) {
                             $scope.keywords.push(result.data.text[i].classification[p].key);
@@ -26,6 +20,10 @@ var articlesController = app.controller("articlesController", ['$scope', '$http'
             }
             console.log($scope.keywords);
         });
+    } else {
+        articleService.getArticles().then(function(e) {
+            $scope.articles = e;
+        })
     }
 
     $scope.toTrustedHTML = function(html) {
---
+++ public/js/services/articleService.js
@@ -0,0 +1,24 @@
+var articleService = app.service('articleService',['$http','$q',function($http,$q){
+  this.getArticles = function(){
+    var articles = [];
+    var deferred = $q.defer();
+    $http.get(app.endpoint).then(function(result) {
+        if (result.status == 200) {
+            articles = result.data.payload;
+            deferred.resolve(articles);
+        }
+    });
+    return deferred.promise;
+  }
+
+  this.getArticle = function(id){
+    var articles = [];
+    var deferred = $q.defer();
+    $http.get(app.endpoint+id).then(function(result) {
+        if (result.status == 200) {
+            deferred.resolve(result);
+        }
+    });
+    return deferred.promise;
+  }
+}]);
